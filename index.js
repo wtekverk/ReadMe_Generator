39c0fd392dfd9acc7abb3964cbc53c5abfc892af
Wendy Tekverk <wtekverk@gmail.com>
2021-02-13 12:11:11 -0700

    added sudo coding to indec.js file

--- index.js
+++ index.js
@@ -4,6 +4,36 @@ const fs = require('fs')
 
 const inquirer = require('inquirer')
 
+inquirer.prompt([
+
+{type: 'input',
+message: 'What is the title of your Project?',
+name: 'title'}
+
+
+
+
+
+
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
+
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+
+
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
+
+
+
+])
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -11,8 +41,28 @@ const questions = [];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
+
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+
 // TODO: Create a function to initialize app
 function init() {}
 
+
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
+
+
 // Function call to initialize app
 init();
+
+
+
+
+
+
+
