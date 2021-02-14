e13e5feac96900a80c36a178539590d4a84a6228
Wendy Tekverk <wtekverk@gmail.com>
2021-02-13 17:08:35 -0700

    moved markdown work and added license functions to generateMArkdowns js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,106 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  if (License == 'none') {
+    return ``
+  } else if (License == 'MIT') {
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else if (License == 'GNU GPLv3') {
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  } else {
 
-`;
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+
+
+}
+
+
+function renderLicenseLink(license) {
+
+  if (License == 'none') {
+    return ``
+  } else if (License == 'MIT') {
+
+    return `https://opensource.org/licenses/MIT`
+  } else if (License == 'GNU GPLv3') {
+
+    return `https://www.gnu.org/licenses/gpl-3.0`
+  } else {
+
+    return `https://opensource.org/licenses/Apache-2.0`
+  }
+
+
+
+
+}
+
+
+function renderLicenseSection(license) {
+  if (License == 'none') {
+    return ``
+  } else{ return `Copyright (c) Licence ${license}`}
+
+
+
+}
+
+function generateMarkdown(response) {
+  return `
+  # ${response.Title}
+  ${renderLicenseBadge(response.License)}
+  
+  ## Description
+  ${response.Description}
+  
+  
+  ## Table of Contents 
+  
+  [Description](##Description)
+  
+  [Installation](##Installation)
+  
+  [Usage](##Usage)
+  
+  [License](##License)
+  
+  [Contributing](##Contributing)
+  
+  [Tests](##Tests)
+  
+  [Questions](##Questions)
+  
+  
+  ## Installation
+  ${response.Installation}
+  
+  
+  ## Usage
+  ${response.Usage}
+  
+  
+  ## License
+  ${renderLicenseSection(response.License)}
+  ${renderLicenseLink(response.License)}
+ 
+  
+  ## Contributing
+  ${response.Contributing}
+  
+  ## Tests
+  ${response.Tests}
+  
+  
+  ## Questions
+  GitHub: (https://${response.GitHub})
+  Email: (https://${response.Email})
+  
+  
+  
+  
+  `;
 }
 
 module.exports = generateMarkdown;
